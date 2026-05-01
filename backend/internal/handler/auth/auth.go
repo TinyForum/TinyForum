6b40be8f00f9875ae9451499dd226cc00a2c4b45
caoyang2002 <cy12968@163.com>
2026-05-01 15:59:57 +0800

    fix [jwt]: same jwt

--- backend/internal/handler/auth/auth.go
+++ backend/internal/handler/auth/auth.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"log"
 	"os"
 	userService "tiny-forum/internal/service/user"
 	"tiny-forum/pkg/response"
@@ -71,6 +72,9 @@ func (h *AuthHandler) Login(c *gin.Context) {
 	if isProduction {
 		cookieValue += "; Secure"
 	}
+	log.Printf("JWT secret used for signing: %s", h.cfg.Private.JWT.Secret)
+log.Printf("JWT secret length: %d", len(h.cfg.Private.JWT.Secret))
+
 	c.Header("Set-Cookie", cookieValue)
 
 	// 响应体只返回用户信息，不暴露 token
